24325dcd13175a667de1c3d59cea00aa0c6a8d1b
Maksym <zzzeleznyakmaxim@gmail.com>
2025-06-05 22:37:48 +0300

    f

--- src/components/api/tmdb.tsx
+++ src/components/api/tmdb.tsx
@@ -1,3 +1,24 @@
+// import axios from "axios";
+// import type { FetchMoviesParams, FetchMoviesResponse } from "../../types/movie";
+
+// export async function fetchMovies({
+//   query,
+//   page = 1,
+// }: FetchMoviesParams): Promise<FetchMoviesResponse> {
+//   const response = await axios.get<FetchMoviesResponse>(
+//     "https://api.themoviedb.org/3/search/movie",
+//     {
+//       params: {
+//         query,
+//         page,
+//       },
+//       headers: {
+//         Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+//       },
+//     }
+//   );
+//   return response.data;
+// }
 import axios from "axios";
 import type { FetchMoviesParams, FetchMoviesResponse } from "../../types/movie";
 
@@ -14,6 +35,7 @@ export async function fetchMovies({
       },
       headers: {
         Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+        Accept: "application/json",
       },
     }
   );
