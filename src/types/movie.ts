44a54b4b2dca25b2bfa403595786209126eda0fe
Maksym <zzzeleznyakmaxim@gmail.com>
2025-06-06 19:33:00 +0300

    F

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -7,3 +7,27 @@ export interface Movie {
   backdrop_path: string;
   vote_average: number;
 }
+
+// Пропси для MovieGrid
+export interface MovieGridProps {
+  movies: Movie[];
+  onSelect: (movie: Movie) => void;
+}
+
+// Пропси для SearchBar
+export interface SearchBarProps {
+  onSubmit: (query: string) => void;
+}
+
+// Параметри для fetchMovies
+export interface FetchMoviesParams {
+  query: string;
+  page?: number;
+}
+
+// Відповідь API TMDB для пошуку фільмів
+export interface FetchMoviesResponse {
+  results: Movie[];
+  total_results: number;
+  total_pages: number;
+}
